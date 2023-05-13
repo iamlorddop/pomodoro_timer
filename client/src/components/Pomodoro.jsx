6a5332bfc3ca8e60138425cf5f81be2f897b95cf
Yulia Khavaeva <88404500+iamlorddop@users.noreply.github.com>
2023-05-13 20:29:59 +0300

    style some pomodoro bg, add buttons settings, start/stop timer and time to pomodoro component

--- client/src/components/Pomodoro.jsx
+++ client/src/components/Pomodoro.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
+library.add(fab)
 
 export default function Pomodoro() {
     return (
         <>
+            <div className='pomodoro-timers-flex-container'>
+                <div className='pomodoro-timers'>
+                    <div className='pomodoro-timer'>
+                        <span className='pomodoro-timer-name'>Pomodoro</span>
+                        <span className='pomodoro-timer-time'>25 : 00</span>
+                    </div>
+                    <div className='pomodoro-timer'>
+                        <span className='pomodoro-timer-name'>Short break</span>
+                        <span className='pomodoro-timer-time'>05 : 00</span>
+                    </div>
+                    <div className='pomodoro-timer'>
+                        <span className='pomodoro-timer-name'>Long break</span>
+                        <span className='pomodoro-timer-time'>15 : 00</span>
+                    </div>
+                </div>
+                <div className='pomodoro-timers-button-container'>
+                    <button className='pomodoro-timers-button setting-button'>
+                        <span>
+                            <FontAwesomeIcon icon="fa-solid fa-gear" />
+                        </span>
+                        Settings
+                    </button>
+                    <button className='pomodoro-timers-button start-or-end-button'>Start timer</button>
+                </div>
+            </div>
             <div className='pomodoro-animation__container'>
                 <p className='text'>
                     Time to focus
